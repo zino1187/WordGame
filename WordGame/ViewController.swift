d5904999c8ce25dca87e14d72d7ac7392c77f4f9
zino <minzino@hanmail.net>
2019-06-16 15:50:10 +0900

    Initial Commit

---
+++ WordGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordGame
+//
+//  Created by zino-mac on 16/06/2019.
+//  Copyright © 2019 zino-mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
